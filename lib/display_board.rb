f15e1c25b7d1adb6917514004223806f38aa7c93
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-06-22 19:57:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,5 @@
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
-  puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
+
 end
 display_board("board")
  puts board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
