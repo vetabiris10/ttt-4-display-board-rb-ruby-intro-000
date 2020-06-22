f8d5e671bbb6c0e9eccb1683889bbfd38ecbe67e
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-06-22 18:54:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
 # Definboarde display_board that accepts a board and prints
 # out the current state.
- def display_board(board=[" "," "," "," "," "," "," "," "," "])
+ def display_board(board)
+   board=[" "," "," "," "," "," "," "," "," "])
